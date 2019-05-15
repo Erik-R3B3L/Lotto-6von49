8761529a1fc5325c9c9d91a75c523c7fd0ad0d16
Erik Rebel <50644136+Erik-R3B3L@users.noreply.github.com>
2019-05-15 02:43:22 +0200

    Add files via upload

---
+++ LottoNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto_Daniel
+{
+    class LottoNumber
+    {
+        int[] lottozahl = new int[49];
+        int[] ziehung = new int[7];
+        List<int> temp = new List<int>();
+        Random zufallszahl = new Random();
+
+        public string getLottoNumbers6Aus49()
+        {
+            string numbers;
+
+
+            for (int y = 1; y < 50; y++)
+            {
+                lottozahl[y - 1] = y;
+            }
+
+
+
+            int i = 0;
+            while (i < 7)
+            {
+                int index = zufallszahl.Next(1, 50);
+                if (!temp.Exists(x => x == index))
+                {
+                    temp.Add(index);
+                    ziehung[i] = index;
+                    i++;
+                }
+            }
+
+            numbers = ziehung[0].ToString() + "  " + ziehung[1].ToString() + "  " + ziehung[2].ToString() + "  " + ziehung[3].ToString() + "  " + ziehung[4].ToString() + "  " + ziehung[5].ToString() + "  Superzahl: " + ziehung[6].ToString();
+
+
+            return numbers;
+
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lotto_Erik_Rebel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Lotto_Erik_Rebel.LottoZahlen lotto = new Lotto_Erik_Rebel.LottoZahlen();
+            NummerReihe1.Content = lotto.getLottoNumbers6Aus49();
+        }
+    }
+}
+
